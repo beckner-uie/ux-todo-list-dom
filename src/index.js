ec60082ef492c63021eeb12263bb5f4d8303714a
beckner-uie <petitepaladin@gmail.com>
2020-08-05 20:44:29 -0400

    created list with checkboxes

--- src/index.js
+++ src/index.js
@@ -22,11 +22,26 @@ function renderTodoApp() {
     let app = document.querySelector("#app")
     let h1 = document.createElement("h1")
     // your code here
+    let toDoList = document.createElement("ul")
+    toDoList.style.listStyleType = "none";
+    
+    for (let index = 0; index < todos.length; index++) {
+        let listItem = todos[index];
+        let check = listItem.completed;
+        let listItemMarkup = `
+        <li>
+        <input type=checkbox name="finished" value="1"> 
+        <label>${listItem.description}</label>
+        </li>
+        `;
+        toDoList.innerHTML += listItemMarkup;
+    }
 
     h1.innerText = "Todo List"
 
     app.appendChild(h1)
     // and maybe some here
+    app.append(toDoList);
 }
 
-renderTodoApp()
+renderTodoApp();
